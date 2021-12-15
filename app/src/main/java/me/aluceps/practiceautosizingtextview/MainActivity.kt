a5ea818398988e530eca56da38fb5cec69d77989
atsushi.takano <atsushi.takano@every.tv>
2021-12-15 15:48:49 +0900

    ViewBindingを有効化

--- app/src/main/java/me/aluceps/practiceautosizingtextview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practiceautosizingtextview/MainActivity.kt
@@ -1,11 +1,13 @@
 package me.aluceps.practiceautosizingtextview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.aluceps.practiceautosizingtextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
