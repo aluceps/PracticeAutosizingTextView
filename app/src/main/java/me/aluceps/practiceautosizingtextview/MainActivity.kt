3bb022e0804a73131bf98cd04fa7b220ecf0c7c1
atsushi.takano <atsushi.takano@every.tv>
2021-12-15 18:03:27 +0900

    autoの属性を追加

--- app/src/main/java/me/aluceps/practiceautosizingtextview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practiceautosizingtextview/MainActivity.kt
@@ -1,13 +1,47 @@
 package me.aluceps.practiceautosizingtextview
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
 import me.aluceps.practiceautosizingtextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val currentValue = MutableLiveData<Float>()
+    private var originalSize = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.textView1.viewTreeObserver.addOnGlobalLayoutListener {
+            setOriginalSize(binding.textView1.width)
+        }
+        setupView()
+        observe()
+    }
+
+    private fun setupView() {
+        binding.slider.addOnChangeListener { _, value, _ ->
+            currentValue.postValue(value * 10)
+        }
+    }
+
+    private fun observe() {
+        currentValue.observe(this) {
+            if (it == null) return@observe
+            Log.d("###", "originalSize: $originalSize currentSize: $it sum: ${originalSize * it}")
+            binding.textView1.layoutParams.width = (originalSize * it).toInt()
+            binding.textView1.requestLayout()
+        }
+    }
+
+    private fun setOriginalSize(value: Int) {
+        if (originalSize == 0) {
+            originalSize = value
+        }
     }
 }
